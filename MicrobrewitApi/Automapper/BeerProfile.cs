89fef8d9db400730abd788cecfba41a6455c1445
John Fredrik Asphaug <john-f@online.no>
2014-03-27 16:26:41 +0100

    Added profiler and a JSON DTo simple version for beer

---
+++ MicrobrewitApi/Automapper/BeerProfile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Microbrewit.Model;
+using Microbrewit.Api.DTOs;
+
+namespace Microbrewit.Api.Automapper
+{
+    public class BeerProfile : Profile
+    {
+        protected override void Configure()
+        {
+            Mapper.CreateMap<Beer, BeerSimpleDto>()
+                .ForMember(dto => dto.Id, conf => conf.MapFrom(rec => rec.Id))
+                .ForMember(dto => dto.Name, conf => conf.MapFrom(rec => rec.Name))
+                .ForMember(dto => dto.ABV, conf => conf.MapFrom(rec => rec.ABV))
+                .ForMember(dto => dto.IBU, conf => conf.MapFrom(rec => rec.IBU))
+                .ForMember(dto => dto.SRM, conf => conf.MapFrom(rec => rec.SRM))
+                .ForMember(dto => dto.Recipe, conf => conf.MapFrom(rec => rec.Recipe))
+                .ForMember(dto => dto.Breweries, conf => conf.MapFrom(rec => rec.Breweries))
+                .ForMember(dto => dto.Brewers, conf => conf.MapFrom(rec => rec.Brewers));
+
+            Mapper.CreateMap<Recipe, DTO>()
+               .ForMember(dto => dto.Name, conf => conf.MapFrom(rec => rec.Beer.Name))
+                .ForMember(dto => dto.Id, conf => conf.MapFrom(rec => rec.Id));
+
+            Mapper.CreateMap<Brewery, DTO>()
+               .ForMember(dto => dto.Name, conf => conf.MapFrom(rec => rec.Name))
+                .ForMember(dto => dto.Id, conf => conf.MapFrom(rec => rec.Id));
+
+            Mapper.CreateMap<User, DTO>()
+               .ForMember(dto => dto.Name, conf => conf.MapFrom(rec => rec.Username));
+
+        }
+    }
+}
---
+++ MicrobrewitApi/DTOs/BeerSimpleDto.cs
@@ -0,0 +1,22 @@
+﻿using Microbrewit.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Microbrewit.Api.DTOs
+{
+    public class BeerSimpleDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ABV ABV { get; set; }
+        public IBU IBU { get; set; }
+        public SRM SRM { get; set; }
+        public DTO Recipe { get; set; }
+
+        public IList<DTO> Breweries { get; set; }
+        public IList<DTO> Brewers { get; set; }
+
+    }
+}
