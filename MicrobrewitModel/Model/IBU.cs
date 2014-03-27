e68535900e7312c1b09975da246705811f60f318
John Fredrik Asphaug <john-f@online.no>
2014-03-27 16:13:10 +0100

    Added beer to ABV IBU and SRM

---
+++ MicrobrewitModel/Model/IBU.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microbrewit.Model
+{
+    public class IBU
+    {
+        public int Id { get; set; }
+        public int Standard { get; set; }
+        public int Formula1 { get; set; }
+        public int Formula2 { get; set; }
+
+        public Beer Beer { get; set; }
+
+    }
+}
