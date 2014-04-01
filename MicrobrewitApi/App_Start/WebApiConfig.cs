82214d97e8234c9300f2ae748a8a827009fde03d
John Fredrik Asphaug <john-f@online.no>
2014-04-01 10:23:36 +0200

    Removed api from path and made json as default return

--- MicrobrewitApi/App_Start/WebApiConfig.cs
+++ MicrobrewitApi/App_Start/WebApiConfig.cs
@@ -30,9 +30,12 @@ namespace Microbrewit.Api
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
            
         }
     }
