fed4a55e6b7aa47e7c6401a65b113d79bf4a0dfc
John Fredrik Asphaug <john-f@online.no>
2014-04-05 15:34:55 +0200

    Created breweryDto for Get resquests

---
+++ MicrobrewitModel/DTOs/BreweryDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microbrewit.Model.DTOs
+{
+    public class BreweryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+
+        public IList<DTO> Members { get; set; }
+        public IList<DTO> Beers { get; set; }
+    }
+}
