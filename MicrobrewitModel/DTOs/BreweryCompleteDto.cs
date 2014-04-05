88bc5d585aec2beb4243cad7380f5736cee7aec5
John Fredrik Asphaug <john-f@online.no>
2014-04-05 15:35:28 +0200

    Created a complete breweryCompleteDto to fit with overall json return.

---
+++ MicrobrewitModel/DTOs/BreweryCompleteDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microbrewit.Model.DTOs
+{
+    public class BreweryCompleteDto
+    {
+        public LinksBrewery Links { get; set; }
+        public IList<BreweryDto> Breweries { get; set; }
+
+        public BreweryCompleteDto()
+        {
+            Links = new LinksBrewery()
+            {
+                Beer = new Links() 
+                {
+                    Href = "http://api.microbrew.it/users/:username",
+                    Type = "user"
+                },
+                User = new Links()
+                {
+                    Href = "http://api.microbrew.it/beers/:id",
+                    Type = "beer"
+                }
+
+            };
+        }
+    }
+}
