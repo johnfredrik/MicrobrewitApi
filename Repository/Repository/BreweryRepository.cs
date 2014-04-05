fb71b77402bbd3ffb5474fa163ac52d33970f98b
John Fredrik Asphaug <john-f@online.no>
2014-04-05 14:48:13 +0200

    Added brewery reposiory and interface

---
+++ Repository/Repository/BreweryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microbrewit.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microbrewit.Repository.Repository
+{
+    public class BreweryRepository : GenericDataRepository<Brewery>, IBeweryRepository
+    {
+    }
+}
