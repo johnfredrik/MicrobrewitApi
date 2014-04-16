fb7938aee278198e5112c1e4a149c9c12374ff07
John Fredrik Asphaug <john-f@online.no>
2014-04-16 10:56:40 +0200

    Changed brewery name to brewery in from of DTO

--- MicrobrewitModel/DTOs/UserPostDto.cs
+++ MicrobrewitModel/DTOs/UserPostDto.cs
@@ -9,7 +9,7 @@ namespace Microbrewit.Model.DTOs
     {
         public string UserName { get; set; }
         public string Email { get; set; }
-        public string BreweryName { get; set; }
+        public DTO Brewery { get; set; }
         public string Settings { get; set; }
         public string Password { get; set; }
     }
