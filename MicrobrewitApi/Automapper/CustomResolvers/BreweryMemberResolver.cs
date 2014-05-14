35f7a659d24254e89f8b74bc9a3917a495315662
John Fredrik Asphaug <john-f@online.no>
2014-05-14 14:31:23 +0200

    Created resolver to add breweryId in BreweryMember

---
+++ MicrobrewitApi/Automapper/CustomResolvers/BreweryMemberResolver.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microbrewit.Model;
+using Microbrewit.Model.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Microbrewit.Api.Automapper.CustomResolvers
+{
+    public class BreweryMemberResolver : ValueResolver<BreweryDto,IList<BreweryMember>>
+    {
+        protected override IList<BreweryMember> ResolveCore(BreweryDto source)
+        {
+            
+            var members = new List<BreweryMember>();
+            foreach (var memberDto in source.Members)
+            {
+                var member = new BreweryMember()
+                {
+                    BreweryId = source.Id,
+                    MemberUsername = memberDto.Username,
+                    Role = memberDto.Role,
+                };
+                members.Add(member);
+            }
+            return members;
+        }
+    }
+}
