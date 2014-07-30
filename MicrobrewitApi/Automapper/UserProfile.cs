40d9f9dd5cd313ebb0b08cca904184617c4cab06
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2014-07-30 14:48:32 +0200

    Added new userprofiles

--- MicrobrewitApi/Automapper/UserProfile.cs
+++ MicrobrewitApi/Automapper/UserProfile.cs
@@ -16,7 +16,10 @@ namespace Microbrewit.Api.Automapper
                 .ForMember(dto => dto.Id, conf => conf.MapFrom(rec => rec.Username))
                 .ForMember(dto => dto.Username, conf => conf.MapFrom(rec => rec.Username))
                 .ForMember(dto => dto.Email, conf => conf.MapFrom(rec => rec.Email))
+                .ForMember(dto => dto.Breweries, conf => conf.MapFrom(rec => rec.Breweries))
+                .ForMember(dto => dto.Beers, conf => conf.MapFrom(rec => rec.Beers))
                 .ForMember(dto => dto.Settings, conf => conf.MapFrom(rec => rec.Settings));
+
 
             Mapper.CreateMap<UserPostDto, User>()
                .ForMember(dto => dto.Username, conf => conf.MapFrom(rec => rec.UserName))
