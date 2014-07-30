9a23bffa51dace3bcda5b7dcbf8475d79d9ab9ff
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2014-07-30 14:52:16 +0200

    Fixed tests after changing origin to OriginsCompleteDto.

--- Microbrewit.Test/OriginControllerTests.cs
+++ Microbrewit.Test/OriginControllerTests.cs
@@ -46,7 +46,7 @@ namespace Microbrewit.Test
         {
             var result = await _controller.GetOrigins();
             Assert.NotNull(result);
-            Assert.NotNull(result.Any());
+            Assert.NotNull(result.Origins.Any());
         }
         [Test]
         public async Task GetOriginValidIdReturnsOk200WithObject()
@@ -67,11 +67,11 @@ namespace Microbrewit.Test
         {
             using (var stream = new StreamReader(JSONPATH + "origin.json"))
             {
-                var count =  _controller.GetOrigins().Result.Count();
+                var count =  _controller.GetOrigins().Result.Origins.Count();
                 string originJson = await stream.ReadToEndAsync();
                 var origin = JsonConvert.DeserializeObject<List<Origin>>(originJson);
                 await _controller.PostOrigin(origin);
-                var total = _controller.GetOrigins().Result.Count();
+                var total = _controller.GetOrigins().Result.Origins.Count();
                 Assert.AreEqual(count + origin.Count, total);
             }
         }
@@ -80,7 +80,7 @@ namespace Microbrewit.Test
         public async Task PutOriginNameGetsUpdated()
         {
             var origins = await _controller.GetOrigins();
-            var origin = origins.FirstOrDefault();
+            var origin = origins.Origins.FirstOrDefault();
             origin.Name = "Bergen";
             await _controller.PutOrigin(origin.Id,origin);
             var updatedOrigin = await _controller.GetOrigin(origin.Id) as OkNegotiatedContentResult<Origin>;
@@ -91,7 +91,7 @@ namespace Microbrewit.Test
         public async Task DeleteOriginReturnsStatusCode200()
         {
             var origins = await _controller.GetOrigins();
-            var origin = origins.SingleOrDefault(o => o.Name.Equals("Norway"));
+            var origin = origins.Origins.SingleOrDefault(o => o.Name.Equals("Norway"));
             var response = await _controller.DeleteOrigin(origin.Id) as OkNegotiatedContentResult<Origin>;
             Assert.IsInstanceOf<OkNegotiatedContentResult<Origin>>(response);
         }
