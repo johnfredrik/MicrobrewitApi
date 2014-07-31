5c4b299bfa42887c88b112d6384ed8cbb2e59a76
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2014-07-31 13:45:52 +0200

    Created mail helper class to send mail.

---
+++ MicrobrewitApi/Util/MailHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace Microbrewit.Api.Util
+{
+    public static class MailHelper
+    {
+        private static string  mailPassword = ConfigurationManager.AppSettings["mailPassword"];
+        public static void SendMail(string subject, string content, string receiver)
+        {
+            SmtpClient smtpClient = new SmtpClient("mail.gandi.net", 587);
+
+            smtpClient.EnableSsl = true;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new System.Net.NetworkCredential("microbrewit@asphaug.io", mailPassword);
+
+            MailMessage mail = new MailMessage();
+
+            //Setting From , To and CC
+            mail.From = new MailAddress("microbrewit@asphaug.io", "Microbrew.it");
+            mail.To.Add(new MailAddress(receiver));
+            //mail.CC.Add(new MailAddress("MyEmailID@gmail.com"));
+            mail.Subject = subject;
+            mail.Body = content;
+
+            smtpClient.Send(mail);
+        }
+    }
+}
