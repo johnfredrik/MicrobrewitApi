372a6f5cf1be0a31ac4d34d5ca75f36557f50a39
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2014-07-31 13:47:15 +0200

    Added UserCredential repository to Unity.

--- MicrobrewitApi/App_Start/WebApiConfig.cs
+++ MicrobrewitApi/App_Start/WebApiConfig.cs
@@ -43,6 +43,7 @@ namespace Microbrewit.Api
             container.RegisterType<ISupplierRepository, SupplierRepository>(new HierarchicalLifetimeManager());
             container.RegisterType<IUserRepository, UserRepository>(new HierarchicalLifetimeManager());
             container.RegisterType<IYeastRepository, YeastRepository>(new HierarchicalLifetimeManager());
+            container.RegisterType<IUserCredentialRepository, UserCredentialRepository>(new HierarchicalLifetimeManager());
            
             
 
