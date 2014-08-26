ec40596fd61d2cda7e8a5742ca1572450f588744
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2014-08-26 13:11:42 +0200

    Added new mappings to DTO

--- MicrobrewitModel/Model/Yeast.cs
+++ MicrobrewitModel/Model/Yeast.cs
@@ -17,9 +17,15 @@ namespace Microbrewit.Model
         public string Flocculation { get; set; }
         public string AlcoholTolerance { get; set; }
         public string ProductCode { get; set; }
-        public string Comment { get; set; }
+        public string Notes { get; set; }
         public string Type { get; set; }
-
+        // new from google doc.
+        public string BrewerySource { get; set; }
+        public string Species { get; set; }
+        public string AttenutionRange { get; set; }
+        public string PitchingFermentationNotes { get; set; }
+       
+        // relations
         public int? SupplierId { get; set; }
         public Supplier Supplier { get; set; }
 
