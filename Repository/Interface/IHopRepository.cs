2507a2e2e82940013ccfde971970d3dc1373c400
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2014-10-30 14:23:15 +0100

    Added GetFrom to get hop froms.

--- Repository/Interface/IHopRepository.cs
+++ Repository/Interface/IHopRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +11,6 @@ namespace Microbrewit.Repository
     public interface IHopRepository : IGenericDataRepository<Hop>
     {
         Flavour AddFlavour(string name);
+        HopForm GetForm(Expression<Func<HopForm, bool>> where, params string[] navigationProperties);
     }
 }
