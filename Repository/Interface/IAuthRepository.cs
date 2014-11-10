4f63eef14ff3473b995e32bc2f68499412eeb058
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2014-11-10 13:22:24 +0100

    Create IAuthRespository.

---
+++ Repository/Interface/IAuthRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microbrewit.Model;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microbrewit.Repository.Interface
+{
+    public interface IAuthRepository
+    {
+        Task<IdentityResult> RegisterUser(UserModel userModel);
+        Task<IdentityUser> FindUser(string userName, string password);
+        Client FindClient(string clientId);
+        Task<bool> AddRefreshToken(RefreshToken token);
+        Task<bool> RemoveRefreshToken(string refreshTokenId);
+        IList<RefreshToken> GetAllRefreshTokens();
+
+    }
+}
