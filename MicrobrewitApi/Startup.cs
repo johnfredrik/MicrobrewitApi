08e6423d82594044baf3220e34f60222da679232
John Fredrik Asphaug <john-f@online.no>
2014-11-21 08:29:33 +0100

    Moved some config files.

--- MicrobrewitApi/Startup.cs
+++ MicrobrewitApi/Startup.cs
@@ -1,23 +1,58 @@
-﻿using System;
+﻿
+
+using System;
+using System.Data.Entity;
+using System.Net;
+using System.Net.Http.Formatting;
 using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.SessionState;
+using Microbrewit.Api.Automapper;
 using Microbrewit.Api.Provider;
+using Microbrewit.HelpPage;
+using Microbrewit.Model;
+using Microbrewit.Model.Migrations;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
+using WebApiContrib.Formatting.Jsonp;
 
 namespace Microbrewit.Api
 {
     public class Startup
     {
+        public static HttpConfiguration HttpConfiguration { get; private set; }
+
+        public Startup()
+        {
+            HttpConfiguration = new HttpConfiguration();
+        }
+
         public void Configuration(IAppBuilder app)
         {
-            HttpConfiguration config = new HttpConfiguration();
+            //HttpConfiguration config = new HttpConfiguration();
+            // Add this code, if not present.
+            AreaRegistration.RegisterAllAreas();
 
+            HttpConfiguration.AddJsonpFormatter();
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            HttpConfiguration.EnableCors();
+
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            AutoMapperConfiguration.Configure();
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MicrobrewitContext, Configuration>());
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<AuthContext, Model.AuthMigration.Configuration>());
             ConfigureOAuth(app);
 
-            WebApiConfig.Register(config);
+            WebApiConfig.Register(HttpConfiguration);
             app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
-            app.UseWebApi(config);
+            app.UseWebApi(HttpConfiguration);
         }
 
         public void ConfigureOAuth(IAppBuilder app)
@@ -35,6 +70,7 @@ namespace Microbrewit.Api
             // Token Generation
             app.UseOAuthAuthorizationServer(OAuthServerOptions);
             app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
 
         }
     }
