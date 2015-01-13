88ccd74c5e58c0c19870b2f9c3f3042432befd3a
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2015-01-13 16:27:47 +0100

    Updated RecipeProgiler

--- MicrobrewitApi/Automapper/RecipeProfile.cs
+++ MicrobrewitApi/Automapper/RecipeProfile.cs
@@ -14,12 +14,9 @@ namespace Microbrewit.Api.Automapper
         protected override void Configure()
         {
             Mapper.CreateMap<Recipe, RecipeDto>()
-                .ForMember(dto => dto.Name, conf => conf.MapFrom(rec => rec.Beer.Name))
-                .ForMember(dto => dto.BeerStyle, conf => conf.MapFrom(rec => rec.Beer.BeerStyle))
                 .ForMember(dto => dto.MashSteps, conf => conf.MapFrom(rec => rec.MashSteps))
                 .ForMember(dto => dto.BoilSteps, conf => conf.MapFrom(rec => rec.BoilSteps))
                 .ForMember(dto => dto.FermentationSteps, conf => conf.MapFrom(rec => rec.FermentationSteps))
-                .ForMember(dto => dto.ForkOf, conf => conf.MapFrom(rec => rec.ForkeOfId))
                  .ForMember(dto => dto.Volume, conf => conf.MapFrom(rec => rec.Volume));
 
             Mapper.CreateMap<Recipe, RecipeSimpleDto>()
@@ -38,7 +35,6 @@ namespace Microbrewit.Api.Automapper
                 .ForMember(dto => dto.MashSteps, conf => conf.ResolveUsing<RecipeMashStepResolver>())
                 .ForMember(dto => dto.BoilSteps, conf => conf.ResolveUsing<RecipeBoilStepResolver>())
                 .ForMember(dto => dto.FermentationSteps, conf => conf.ResolveUsing<RecipeFermentationStepResolver>())
-                .ForMember(dto => dto.ForkeOfId, conf => conf.MapFrom(rec => rec.ForkOf))
                 .ForMember(dto => dto.Volume, conf => conf.MapFrom(rec => rec.Volume));
             
         }
