79dfb563d6cedf5977d1ff2113647345b1fab054
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2015-01-14 08:13:49 +0100

    Added more profiles
    Added profiles for Geoloc and gravatar on brewery users.

--- MicrobrewitApi/Automapper/BreweryProfile.cs
+++ MicrobrewitApi/Automapper/BreweryProfile.cs
@@ -19,11 +19,14 @@ namespace Microbrewit.Api.Automapper
                 .ForMember(dto => dto.Description, conf => conf.MapFrom(rec => rec.Description))
                 .ForMember(dto => dto.Type, conf => conf.MapFrom(rec => rec.Type))
                 .ForMember(dto => dto.Members, conf => conf.MapFrom(rec => rec.Members))
-                .ForMember(dto => dto.Beers, conf => conf.MapFrom(rec => rec.Beers));
+                .ForMember(dto => dto.Beers, conf => conf.MapFrom(rec => rec.Beers))
+                .ForMember(dto => dto.GeoLocation, conf => conf.ResolveUsing<GeoLocationResolver>());
 
 
             Mapper.CreateMap<BreweryMember, DTOUser>()
-                .ForMember(dto => dto.Username, conf => conf.MapFrom(rec => rec.MemberUsername));
+                .ForMember(dto => dto.Username, conf => conf.MapFrom(rec => rec.MemberUsername))
+                .ForMember(dto => dto.Role, conf => conf.MapFrom(rec => rec.Role))
+                .ForMember(dto => dto.Gravatar, conf => conf.MapFrom(rec => rec.Member.Gravatar));
 
             Mapper.CreateMap<BreweryMember, BreweryDto>()
                 .ForMember(dto => dto.Name, conf => conf.MapFrom(rec => rec.Brewery.Name))
@@ -40,7 +43,9 @@ namespace Microbrewit.Api.Automapper
                 .ForMember(dto => dto.Description, conf => conf.MapFrom(rec => rec.Description))
                 .ForMember(dto => dto.Type, conf => conf.MapFrom(rec => rec.Type))
                 .ForMember(dto => dto.Members, conf => conf.ResolveUsing<BreweryMemberResolver>())
-                .ForMember(dto => dto.Beers, conf => conf.MapFrom(rec => rec.Beers));
+                .ForMember(dto => dto.Beers, conf => conf.MapFrom(rec => rec.Beers))
+                 .ForMember(dto => dto.Latitude, conf => conf.MapFrom(rec => rec.GeoLocation.Latitude))
+                .ForMember(dto => dto.Longitude, conf => conf.MapFrom(rec => rec.GeoLocation.Longitude));
 
             Mapper.CreateMap<DTOUser, BreweryMember>()
                .ForMember(dto => dto.MemberUsername, conf => conf.MapFrom(rec => rec.Username))
