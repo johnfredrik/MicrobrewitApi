1a23a7d3ca3b704ee38be45f22b3c18028a18847
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2015-01-15 10:48:33 +0100

    Created a GeoLocationDto.

---
+++ MicrobrewitModel/DTOs/GeoLocationDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nest;
+using Newtonsoft.Json;
+
+namespace Microbrewit.Model.DTOs
+{
+    [ElasticType(Name = "beer")]
+    public class GeoLocationDto
+    {
+        [JsonProperty(PropertyName = "latitude")]
+        public double Latitude { get; set; }
+        [JsonProperty(PropertyName = "longitude")]
+        public double Longitude { get; set; }
+    }
+}
