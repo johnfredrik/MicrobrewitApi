25be6f43ef4ac1104e6037bd3ed8b88ffa5b4ab6
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2015-01-19 13:01:35 +0100

    Starting a test class for fermentables.

---
+++ Microbrewit.Test/Repository/FermentableRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microbrewit.Repository;
+using NUnit.Framework;
+
+namespace Microbrewit.Test.Repository
+{
+    [TestFixture]
+    class FermentableRepository
+    {
+        private static IFermentableRepository _fermentableRepository = new Microbrewit.Repository.FermentableRepository();
+
+        [Test]
+        public void GetAll()
+        {
+            var fermentables = _fermentableRepository.GetSingle(f => f.Id == 143,"Supplier.Origin","SubFermentables");
+
+        }
+    }
+}
