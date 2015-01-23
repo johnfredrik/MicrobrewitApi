22b7e7f77a4341fbb8a41321476723378b0c5633
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2015-01-23 08:17:07 +0100

    New Test try.
    Got to try harder.

---
+++ Microbrewit.Test/Calculations/AbvCalculationTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microbrewit.Api.Areas.HelpPage;
+using NUnit.Framework;
+using Microbrewit.Api.Util;
+
+namespace Microbrewit.Test.Calculations
+{
+    [TestFixture]
+    public class AbvCalculationTests
+    {
+        [Test]
+        public void CalculateMillerABV()
+        {
+            var miller = Math.Round(Formulas.MillerABV(1.05, 1.01),2);
+            Assert.AreEqual(5.33, miller);
+        }
+
+        [Test]
+        public void CalculateSimpleABV()
+        {
+            var simple = Math.Round(Formulas.SimpleABV(1.05, 1.01), 2);
+            Assert.AreEqual(5.25,simple);
+        }
+
+        [Test]
+        public void CalculateAdvancedABV()
+        {
+            var advanced = Math.Round(Formulas.AdvancedABV(1.05, 1.01),2);
+            Assert.AreEqual(5.19,advanced);     
+        }
+
+
+    }
+}
