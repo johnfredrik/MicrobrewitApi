17a6f62d29f43730c8591cf082acab770d52e745
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2015-01-23 07:24:18 +0100

    Changed and added some new methods
    Changed name of GetallUserBeer to GetAllUserBeerAsync. Also added none
    async method. Added method to get all beers for a brewery.

--- Repository/Interface/IBeerRepository.cs
+++ Repository/Interface/IBeerRepository.cs
@@ -10,6 +10,9 @@ namespace Microbrewit.Repository
     public interface IBeerRepository : IGenericDataRepository<Beer>
     {
         Task<IList<Beer>> GetLastAsync(int from, int size, params string[] navigationProperties);
-        Task<IList<Beer>> GetAllUserBeer(string username, params string[] navigationProperties);
+        Task<IList<Beer>> GetAllUserBeerAsync(string username, params string[] navigationProperties);
+        IList<Beer> GetAllUserBeer(string username, params string[] navigationProperties);
+        IList<Beer> GetAllBreweryBeers(int breweryId, params string[] navigationProperties);
+
     }
 }
