36c5a0c9888dda3589573cf6e52152da2eadbcdb
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2015-01-23 08:39:28 +0100

    Move and Start of HopService

--- Microbrewit.Service/Component/HopService.cs
+++ Microbrewit.Service/Component/HopService.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Microbrewit.Model.DTOs;
+using Microbrewit.Service.Interface;
 
 namespace Microbrewit.Service.Component
 {
-    public class HopService
+    public class HopService : IHopService
     {
+        public Task<IList<HopDto>> GetHopsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HopDto> GetHopAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
