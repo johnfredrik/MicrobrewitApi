463800170268e801ef6b69c6a061b98adf5a277b
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2015-02-09 18:55:27 +0100

    Added Blob storage streaming provider.

---
+++ MicrobrewitApi/BlobStorageMultipartStreamProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Microbrewit.Api
+{
+    public class BlobStorageMultipartStreamProvider : MultipartStreamProvider
+    {
+        public override Stream GetStream(HttpContent parent, HttpContentHeaders headers)
+        {
+            Stream stream = null;
+            ContentDispositionHeaderValue contentDisposition = headers.ContentDisposition;
+            if (contentDisposition != null)
+            {
+                if (!String.IsNullOrWhiteSpace(contentDisposition.FileName))
+                {
+                    string connectionString = ConfigurationManager.AppSettings["azureConnectionString"];
+                    string containerName = ConfigurationManager.AppSettings["container"];
+                    CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+                    CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+                    CloudBlobContainer blobContainer = blobClient.GetContainerReference(containerName);
+
+
+                    CloudBlockBlob blob = blobContainer.GetBlockBlobReference(Guid.NewGuid().ToString() + ".jpg");
+                    stream = blob.OpenWrite();
+                    headers.ContentDisposition.FileName = blob.Name;
+                }
+            }
+            return stream;
+        }
+    }
+}
