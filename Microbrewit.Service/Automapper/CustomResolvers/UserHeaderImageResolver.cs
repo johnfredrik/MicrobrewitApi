5b26d00e8b304e81285a3a175f553e54b7095a33
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2015-02-12 07:27:40 +0100

    Created resolver for userHeader

---
+++ Microbrewit.Service/Automapper/CustomResolvers/UserHeaderImageResolver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microbrewit.Model;
+using Microbrewit.Model.DTOs;
+using Microbrewit.Repository;
+using Microbrewit.Service.Elasticsearch.Component;
+using Microbrewit.Service.Elasticsearch.Interface;
+
+namespace Microbrewit.Service.Automapper.CustomResolvers
+{
+    public class UserHeaderImageResolver : ValueResolver<UserDto, string>
+    {
+        protected override string ResolveCore(UserDto source)
+        {
+            if (source.HeaderImage == null) return string.Empty;
+            var image = source.HeaderImage.Split('/').LastOrDefault();
+            return image;
+        }
+    }
+}
