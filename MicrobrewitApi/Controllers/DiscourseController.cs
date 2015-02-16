779e601a01ec67aa3cbd4f3be41a98c4ad2dda2d
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2015-02-16 15:51:24 +0100

    New discoursecontroller to handle discourse stuff.

---
+++ MicrobrewitApi/Controllers/DiscourseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Microbrewit.Api.Controllers
+{
+    [RoutePrefix("discourse")]
+    public class DiscourseController : ApiController
+    {
+        [HttpGet]
+        public void ValidateDiscourse(string sso, string sig)
+        {
+            
+        } 
+    }
+}
