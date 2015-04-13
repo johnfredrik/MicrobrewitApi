fb58fbd29a34596b1a5e99549e614c01403cf77d
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2015-04-13 08:19:26 +0200

    Moving away from generic repository.

--- Repository/Interface/IHopRepository.cs
+++ Repository/Interface/IHopRepository.cs
@@ -8,8 +8,22 @@ using System.Threading.Tasks;
 
 namespace Microbrewit.Repository
 {
-    public interface IHopRepository : IGenericDataRepository<Hop>
+    public interface IHopRepository
     {
+        IList<Hop> GetAll(params string[] navigationProperties);
+        IList<Hop> GetList(Expression<Func<Hop, bool>> where, params string[] navigationProperties);
+        Hop GetSingle(Func<Hop, bool> where, params string[] navigationProperties);
+        void Add(Hop item);
+        void Update(Hop item);
+        void Remove(Hop item);
+
+        //Async methods
+        Task<IList<Hop>> GetAllAsync(params string[] navigationProperties);
+        Task<Hop> GetSingleAsync(Expression<Func<Hop, bool>> where, params string[] navigtionProperties);
+        Task AddAsync(Hop item);
+        Task<int> UpdateAsync(Hop item);
+        Task RemoveAsync(Hop item);
+
         Flavour AddFlavour(string name);
         HopForm GetForm(Expression<Func<HopForm, bool>> where, params string[] navigationProperties);
         Task<IList<HopForm>> GetHopFormsAsync();
