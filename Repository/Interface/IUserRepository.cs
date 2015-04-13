d2d4b18743c14f9272061d08d2ef54dcd20b84ae
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2015-04-13 08:17:42 +0200

    Added new methods.

--- Repository/Interface/IUserRepository.cs
+++ Repository/Interface/IUserRepository.cs
@@ -4,11 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microbrewit.Model.DTOs;
 
 namespace Microbrewit.Repository
 {
     public interface IUserRepository : IGenericDataRepository<User>
     {
         IEnumerable<UserSocial> GetUserSocials(string username);
+        Task<IEnumerable<UserBeer>> GetAllUserBeersAsync(string username);
+        Task<bool> ConfirmBreweryMemberAsync(string username, NotificationDto notificationDto);
+        Task<bool> ConfirmUserBeerAsync(string username, NotificationDto notificationDto);
     }
 }
