ccae117afd6c6b7f04e74bfd76fd0d8d0c7c9a74
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2015-05-05 12:44:15 +0200

    Changed OriginRepository not not use generic repository

--- Repository/Interface/IOriginRespository.cs
+++ Repository/Interface/IOriginRespository.cs
@@ -2,12 +2,25 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Microbrewit.Repository
 {
-    public interface IOriginRespository : IGenericDataRepository<Origin>
+    public interface IOriginRespository
     {
+        IList<Origin> GetAll(params string[] navigationProperties);
+        Origin GetSingle(int id, params string[] navigationProperties);
+        void Add(params Origin[] items);
+        void Update(params Origin[] items);
+        void Remove(params Origin[] items);
+
+        //Async methods
+        Task<IList<Origin>> GetAllAsync(params string[] navigationProperties);
+        Task<Origin> GetSingleAsync(Expression<Func<Origin, bool>> where, params string[] navigtionProperties);
+        Task AddAsync(params Origin[] items);
+        Task<int> UpdateAsync(params Origin[] items);
+        Task RemoveAsync(params Origin[] items);
     }
 }
--- Repository/Repository/OriginRepository.cs
+++ Repository/Repository/OriginRepository.cs
@@ -1,13 +1,218 @@
 ﻿using Microbrewit.Model;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using log4net;
 
 namespace Microbrewit.Repository
 {
-    public class OriginRepository : GenericDataRepository<Origin>, IOriginRespository
+    public class OriginRepository : IOriginRespository
     {
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public virtual IList<Origin> GetAll(params string[] navigationProperties)
+        {
+            List<Origin> list;
+            using (var context = new MicrobrewitContext())
+            {
+                IQueryable<Origin> dbQuery = context.Set<Origin>();
+
+                //Apply eager loading
+                foreach (string navigationProperty in navigationProperties)
+                    dbQuery = dbQuery.Include<Origin>(navigationProperty);
+
+                list = dbQuery
+                    .AsNoTracking()
+                    .ToList<Origin>();
+            }
+            return list;
+        }
+
+
+
+        public virtual Origin GetSingle(int id, params string[] navigationProperties)
+        {
+            Origin item = null;
+            using (var context = new MicrobrewitContext())
+            {
+                IQueryable<Origin> dbQuery = context.Set<Origin>();
+
+                //Apply eager loading
+                foreach (string navigationProperty in navigationProperties)
+                    dbQuery = dbQuery.Include<Origin>(navigationProperty);
+
+                item = dbQuery
+                    .AsNoTracking() //Don't track any changes for the selected item
+                    .FirstOrDefault(o => o.OriginId == id); //Apply where clause
+            }
+            return item;
+        }
+
+        public virtual void Add(params Origin[] items)
+        {
+            using (var context = new MicrobrewitContext())
+            {
+                foreach (Origin item in items)
+                {
+                    context.Entry(item).State = EntityState.Added;
+                }
+
+                try
+                {
+                    context.SaveChanges();
+                }
+                catch (DbEntityValidationException dbEx)
+                {
+                    foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+                            Trace.TraceInformation("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
+                            Log.DebugFormat("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
+                        }
+                    }
+                }
+            }
+        }
+
+        public virtual void Update(params Origin[] items)
+        {
+            using (var context = new MicrobrewitContext())
+            {
+                foreach (Origin item in items)
+                {
+                    context.Entry(item).State = EntityState.Modified;
+                }
+                context.SaveChanges();
+            }
+        }
+
+        public virtual void Remove(params Origin[] items)
+        {
+            using (var context = new MicrobrewitContext())
+            {
+                foreach (Origin item in items)
+                {
+                    context.Entry(item).State = EntityState.Deleted;
+                }
+                context.SaveChanges();
+            }
+        }
+
+        public virtual async Task<IList<Origin>> GetAllAsync(params string[] navigationProperties)
+        {
+            using (var context = new MicrobrewitContext())
+            {
+
+                IQueryable<Origin> dbQuery = context.Set<Origin>();
+
+                //Apply eager loading
+                foreach (string navigationProperty in navigationProperties)
+                {
+                    dbQuery = dbQuery.Include<Origin>(navigationProperty);
+                }
+                return await dbQuery.ToListAsync();
+            }
+        }
+
+        public virtual async Task<Origin> GetSingleAsync(Expression<Func<Origin, bool>> where, params string[] navigationProperties)
+        {
+            using (var context = new MicrobrewitContext())
+            {
+
+                IQueryable<Origin> dbQuery = context.Set<Origin>();
+
+                //Apply eager loading
+                dbQuery = navigationProperties.Aggregate(dbQuery, (current, navigationProperty) => current.Include<Origin>(navigationProperty));
+
+                return await dbQuery.SingleOrDefaultAsync(where);
+            }
+        }
+
+        public virtual async Task AddAsync(params Origin[] items)
+        {
+            using (var context = new MicrobrewitContext())
+            {
+
+                foreach (Origin item in items)
+                {
+                    context.Entry(item).State = EntityState.Added;
+                }
+
+                try
+                {
+                    await context.SaveChangesAsync();
+                }
+                catch (DbEntityValidationException dbEx)
+                {
+                    //foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    //{
+                    //    foreach (var validationError in validationErrors.ValidationErrors)
+                    //    {
+                    //        Trace.TraceInformation("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
+                    //        Log.DebugFormat("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
+                    //        throw dbEx;
+                    //    }
+                    //}
+                    throw;
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
+
+        }
+
+        public virtual async Task<int> UpdateAsync(params Origin[] items)
+        {
+            using (var context = new MicrobrewitContext())
+            {
+
+                foreach (Origin item in items)
+                {
+
+                    context.Entry(item).State = EntityState.Modified;
+
+                }
+                try
+                {
+                    return await context.SaveChangesAsync();
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+
+            }
+        }
+
+        public virtual async Task RemoveAsync(params Origin[] items)
+        {
+            using (var context = new MicrobrewitContext())
+            {
+
+                foreach (Origin item in items)
+                {
+                    context.Entry(item).State = EntityState.Deleted;
+
+                }
+                try
+                {
+                    await context.SaveChangesAsync();
+                }
+                catch (Exception e)
+                {
+                    Log.Debug(e);
+                    throw;
+                }
+            }
+        }
     }
 }
