31d598613babcd854cbc1c7b814a8529fe2789c2
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2015-05-05 12:42:39 +0200

    Added dapper helper class to generate sql connection

---
+++ Repository/DapperHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microbrewit.Repository
+{
+    public static class DapperHelper
+    {
+        private static readonly string SqlConnection =  ConfigurationManager.ConnectionStrings["MicrobrewitContext"].ConnectionString;
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(SqlConnection);
+        }
+    }
+}
